adc6ebc734d142ce37eabc62dc66629a0b0094e2
Ayush singh <aayushssingh2003@gmail.com>
2025-07-05 02:13:31 +0530

    update and add postcard component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,88 @@
-import react from "react" 
+
 import {Logoutbtn , Logo , Container} from './index'
+import { Link, Links } from "react-router";
+import { useSelector , useDispatch} from "react-redux";
+import { useNavigate } from "react-router";
+
+
+
 
 
 function Header (){
-    return (
-        <>
-        Header
+    const authStatus =  useSelector((state) => state.auth.status) 
+    const navigate = useNavigate()
 
-        </>
+    const navItems = [
+        {
+          name: 'Home',
+          slug: "/",
+          active: true
+        }, 
+        {
+          name: "Login",
+          slug: "/login",
+          active: !authStatus,
+      },
+      {
+          name: "Signup",
+          slug: "/signup",
+          active: !authStatus,
+      },
+      {
+          name: "All Posts",
+          slug: "/all-posts",
+          active: authStatus,
+      },
+      {
+          name: "Add Post",
+          slug: "/add-post",
+          active: authStatus,
+      },
+      ]
+
+
+
+
+
+    return (
+        <header className="py-3 shadow bg-gray-600">
+            <Container>
+            <nav className="flex">
+                <div className="mr-4">
+            <link to = "/">
+            <Logo />
+            </link>  
+            </div >    
+            <ul className="flex ml-auto">
+                {navItems.map((item)=> (
+                    item.active ? (
+                <li key = {item.name}>
+                 <button
+                className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                 onClick = {() => navigate(item.slug)} 
+                 >
+                    {item.name}
+                 </button>
+
+
+                </li>
+
+                    ) : null
+                )
+
+                )}
+                {authStatus && (
+                    <li>
+                        <Logoutbtn/>
+                    </li>
+                )}
+
+            </ul>
+            </nav>
+            </Container>
+      
+
+        </header>
     )
 }
 
