ffecb5130894e023de1eb2025ca3d553a05e88d6
Ayush singh <aayushssingh2003@gmail.com>
2025-07-11 17:13:57 +0530

    update

--- src/Components/RTE.jsx
+++ src/Components/RTE.jsx
@@ -13,7 +13,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-         apiKey='' // Remove API key for self-hosted version
+         apiKey="aamhh4uo3u2b01tajscv08sqj8ztan4kcfcmkrpdp4jl2pe6"
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
